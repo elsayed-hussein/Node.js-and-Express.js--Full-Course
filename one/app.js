ac15abc7ddbc7fa5dc73c69c4df41c624a8024de
elsayed-hussein <elsayed.hussen@gmail.com>
2023-02-17 20:51:31 +0200

    modules start

--- one/app.js
+++ one/app.js
@@ -1,10 +1,15 @@
-const amount = 12;
-if (amount < 10) {
-  console.log("small number");
-} else {
-  console.log("large number");
-}
-console.log(`first node app is in : ${__dirname} \nnamed : ${__filename}`);
-setInterval(() => {
-  console.log("helle world!");
-}, 1000);
+// const amount = 12;
+// if (amount < 10) {
+//   console.log("small number");
+// } else {
+//   console.log("large number");
+// }
+// console.log(`first node app is in : ${__dirname} \nnamed : ${__filename}`);
+// setInterval(() => {
+//   console.log("helle world!");
+// }, 1000);
+// =================================================
+// *************************************************
+// ###################  Modules  ###################
+// *************************************************
+// =================================================
